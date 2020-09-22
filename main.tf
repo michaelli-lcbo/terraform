5c501f16aa498fc427e9d6b8236ca387876f17f3
michaelli-lcbo <54758344+michaelli-lcbo@users.noreply.github.com>
2020-09-22 17:05:46 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
++  backend "remote" {
++    organization = "sanheli"
++    workspaces {
++      name = "Example-Workspace"
++    }
++  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
